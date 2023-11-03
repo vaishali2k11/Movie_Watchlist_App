353b7430eea74f8264e7f3c824387ed97c961dc9
vaishali2k11 <rathore.vaishali1011@gmail.com>
2023-11-03 16:10:15 +0530

    Implemented all the functionality in the application

--- movie_watchlist_app/src/index.js
+++ movie_watchlist_app/src/index.js
@@ -4,15 +4,20 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+let persistor = persistStore(store);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
